d516b365f3930f75e23813751a62d8c4d3c68b75
dmilon <37899667+dmilon@users.noreply.github.com>
2019-04-02 14:16:27 +0200

    Implemented ActiveCity container

---
+++ src/containers/active_city.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class ActiveCity extends React.Component {
+  render () {
+    return (
+      <div>Hello from ActiveCity</div>
+    )
+  }
+}
+
+export default ActiveCity
